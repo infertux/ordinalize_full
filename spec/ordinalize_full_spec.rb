8d898e896b5c37ba0b03858c413366b6ec9a776e
Cédric Félizard <cedric@felizard.fr>
2014-07-15 12:33:53 +0000

    Add spec for localized short ordinal number
    e.g. 1st, 2nd in English and 1er, 2ème in French

--- spec/ordinalize_full_spec.rb
+++ spec/ordinalize_full_spec.rb
@@ -3,6 +3,8 @@ require "ordinalize_full/integer"
 describe OrdinalizeFull do
   describe "#ordinalize_in_full" do
     context "with the default locale (:en)" do
+      before { I18n.locale = :en }
+
       specify { expect(1.ordinalize_in_full).to eq("first") }
       specify { expect(42.ordinalize_in_full).to eq("forty second") }
     end
@@ -13,10 +15,36 @@ describe OrdinalizeFull do
       specify { expect(1.ordinalize_in_full).to eq("premier") }
       specify { expect(42.ordinalize_in_full).to eq("quarante-deuxième") }
     end
+
+    it "raises for unknown locales" do
+      I18n.locale = :zz
+      expect { 1.ordinalize_in_full }.to raise_error(NotImplementedError)
+    end
   end
 
   describe "#ordinalize_full" do
     specify { expect(1.ordinalize_full).to eq(1.ordinalize_in_full) }
+  end
+
+  describe "#ordinalize" do
+    context "with the default locale (:en)" do
+      before { I18n.locale = :en }
+
+      specify { expect(1.ordinalize(in_full: true)).to eq("first") }
+      specify { expect(1.ordinalize(in_full: false)).to eq("1st") }
+    end
+
+    context "with locale = :fr" do
+      before { I18n.locale = :fr }
+
+      specify { expect(1.ordinalize(in_full: true)).to eq("premier") }
+      specify { expect(1.ordinalize(in_full: false)).to eq("1er") }
+    end
+
+    it "raises for unknown locales" do
+      I18n.locale = :zz
+      expect { 1.ordinalize(in_full: false) }.to raise_error(NotImplementedError)
+    end
   end
 end
 
